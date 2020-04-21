a3e929822b7985dab950e0458b463dacacc75eac
VaskillerDev <Vaskoooo9241@gmail.com>
2020-04-21 13:12:15 +0300

    rewrite parser system. add Rule struct

--- src/text_processing/parser/states.rs
+++ src/text_processing/parser/states.rs
@@ -1,131 +1,196 @@
 extern crate regex;
 use crate::environment::logger::Logger;
-use crate::text_processing::ast::types::{FuncType, Util, DataType};
-use regex::*;
+use crate::text_processing::ast::types::{
+    ArgumentGroup, BinaryExpr, DataType, DataVar, FuncType, UnaryFuncExpr, Util,
+};
 use std::borrow::Borrow;
+use std::iter::FromIterator;
+use std::str::from_utf8;
 
-// It's not time yet for use
-// maybe useless abstraction.
-enum ParserState {
-    Start,
-    Done,
-    Exception(String),
-}
+// rule for parse  DSL-line from string
+// it's struct promotes pipeline logic for create UnaryFuncExpression
+// please, see next schedule:
+// UnaryFuncExpression = get_func_type + get_channels + get_expressions
+pub struct Rule;
 
-// It's use for parse string line with command.
-// Implement ways form create abstract syntax tree from string-based values.
-struct Parser;
-
-// warning: using regexp - it's not good solution.
-// exec time: ~ 129ms
-// Implement on lexer-base solution in future release.
-// TODO: implement
-impl Parser {
-    // translate string to UnaryFuncExpr
-    pub fn from_unary_func_expr(line: String) {
-        lazy_static! {
-            static ref RE_UNARY_FUNC_EXPR_PARSER : Regex = Regex::new(
-                r"(?P<func>on[\w\s]*)(?P<channels>\(['\w\s,]*\))(?P<expressions>\((['\s\w,]*(?:==|<|>|>=|<=|!=)[\s\w']*,?)+\))?(?P<statements>\([\w\s=':,]*\))?"
-            ).unwrap();
-            static ref RE_BINARY_EXPR_PARSER :Regex = Regex::new(
-                r"(?P<lterm>['\w]+)[\s]*(?P<oprt>(?:>=|<=|>|<|==|!=))[\s]*(?P<rterm>['\w]+)"
-            ).unwrap();
-        }
-        let res: Captures = RE_UNARY_FUNC_EXPR_PARSER.captures(line.as_str()).unwrap();
-
-        // regexp names group:
-        let func = &res.name("func");
-        let channels = &res.name("channels");
-        let expressions = &res.name("expressions");
-        let statements = &res.name("statements");
-
-        // extract fields from string
-        match func {
-            Some(val) => {
-                let func_type = FuncType::from_string(val.as_str().to_lowercase());
-                println!("{:?}", func_type) //TODO: continue
+impl Rule {
+    // helping to split line on string group for next processing
+    pub fn split_on_raw_group<T: Into<String>>(line: T) -> Vec<String> {
+        let mut line: String = line.into();
+        let mut capture_mode: bool = true;
+        line.retain(|e: char| {
+            if e == '\'' {
+                capture_mode = !capture_mode
             }
-            None => Logger::error("Function name not found."),
-        }
-        match channels {
-            Some(val) => {
-                let channels: Vec<&str> = val
-                    .as_str()
-                    .trim_matches('(')
-                    .trim_matches(')')
-                    .split(",")
-                    .collect();
-                let mut is_valid = true;
+            if capture_mode {
+                !e.is_whitespace()
+            } else {
+                true
+            }
+        });
+        let lines: Vec<&[u8]> = line
+            .as_bytes()
+            .split(|elem| *elem == b'\n' || *elem == b';')
+            .collect();
 
-                'checker: for channel in &channels {
-                    if !Util::is_single_word(channel.to_string()) {
-                        Logger::error(("Channel name error in :".to_owned() + channel).as_ref());
-                        is_valid = false;
-                        break 'checker;
+        let mut accum: Vec<Vec<&[u8]>> = vec![vec![]];
+        for line in lines {
+            let mut capture_mode = true;
+            let a: Vec<&[u8]> = line
+                .split(|elem| {
+                    if *elem == b'\'' {
+                        capture_mode = !capture_mode
                     }
-                }
-
-                if is_valid {
-                    let channels: Vec<String> =
-                        channels.iter().map(|channel| channel.to_string()).collect();
-                    println!("{:?}", &channels)
-                }
+                    (*elem == b'(' || *elem == b')') && capture_mode == true
+                })
+                .collect();
+            if !a.is_empty() {
+                accum.push(a);
             }
-            None => Logger::error("Channels name not found."),
         }
-        match expressions {
-            Some(val) => {
-                let expressions: Vec<&str> = val
-                    .as_str()
-                    .trim_matches('(')
-                    .trim_matches(')')
-                    .split(",")
-                    .collect();
+        let accum: Vec<String> = accum
+            .iter()
+            .flatten()
+            .map(|e| from_utf8(e).unwrap().to_owned())
+            .filter(|e| !e.is_empty())
+            .collect();
 
-                for expression in expressions {
-                    let res: Captures = RE_BINARY_EXPR_PARSER.captures(expression.trim()).unwrap();
-                    let lterm = &res.name("lterm").unwrap().as_str();
-                    let rterm = &res.name("rterm").unwrap().as_str();
-                    let oprt = &res.name("oprt").unwrap().as_str();
-                    //TODO: continue
-                }
+        accum
+    }
+    // helping to split line on binary expression (used in get_expressions)
+    fn split_expression(raw_expression: &str) -> Option<BinaryExpr> {
+        fn split_on_data_type_and_operator(line: &str, substr: &str) -> Option<Vec<String>> {
+            if line.contains(substr) {
+                return Some(
+                    line.split(substr)
+                        .map(|e: &str| e.to_owned())
+                        .collect::<Vec<String>>(),
+                );
             }
-            None => (),
+            None
         }
-        match statements {
-            Some(val) => {
-                let statements: Vec<&str> = val
-                    .as_str()
-                    .trim_matches('(')
-                    .trim_matches(')')
-                    .split(",")
-                    .collect();
-                let statements: Vec<&str> = statements
-                    .iter()
-                    .map(|statement| statement.trim())
-                    .collect();
-                println!("{:?}", statements); //TODO: continue
-            }
-            None => (),
+        fn create_data_type(term: &String) -> Option<DataType> {
+            DataType::from_string(term, &Util::identify_type(term))
+        }
+        let operators = vec!["==", "!=", ">=", "<=", ">", "<"];
+        let mut binary_expression: Option<BinaryExpr> = None;
+        'a: for operator in operators {
+            match split_on_data_type_and_operator(raw_expression, operator) {
+                Some(ref val) => {
+                    binary_expression = Some(BinaryExpr::new(
+                        create_data_type(&val[0]).unwrap(),
+                        create_data_type(&val[1]).unwrap(),
+                        operator.to_string(),
+                    ));
+                    break 'a;
+                }
+                None => binary_expression = None,
+            };
+        }
+        if binary_expression.is_none() {
+            Logger::error(
+                format!("error parse in binary expression at: {}", raw_expression).as_str(),
+            )
+        }
+        binary_expression
+    }
+
+    fn split_statement(raw_statement: &str) -> DataVar {
+        let val: Vec<&str> = raw_statement.splitn(2, ":").collect();
+        let type_and_value: Vec<&str> = val[1].split("=").collect();
+
+        if type_and_value.len() == 2 {
+            let (symbol, raw_type, value) = (val[0], type_and_value[0], type_and_value[1]);
+            let data_type = DataType::from_string(value, raw_type).expect(
+                format!(
+                    "DataType creation has been failed at {}.\n  value: {} type: {}",
+                    symbol, type_and_value[0], type_and_value[1]
+                )
+                .as_str(),
+            );
+            DataVar::new(symbol.to_string(), data_type)
+        } else {
+            let (symbol, raw_type) = (val[0], type_and_value[0]);
+            let data_type = DataType::from_type_default_value(raw_type).expect(
+                format!(
+                    "DataType creation has been failed at {}.\n value: {} type: {}",
+                    symbol, type_and_value[0], type_and_value[1]
+                )
+                .as_str(),
+            );
+            DataVar::new(symbol.to_string(), data_type)
         }
     }
+
+    pub fn get_argument_groups<T: IntoIterator<Item = String>>(
+        collection: T,
+    ) //-> Vec<ArgumentGroup>
+    {
+        // TODO: continue
+        /*let accum : Vec<Vec<ArgumentGroup>> = vec![vec![]];
+        collection
+            .into_iter()
+            .fold(Vec::new(Vec<ArgumentGroup>),|e|{})*/
+    }
+
+    pub fn get_func_type<T: ToString>(val: T) -> Option<FuncType> {
+        FuncType::from_string(val.to_string())
+    }
+
+    pub fn get_channels<T: ToString>(val: T) -> Option<Vec<DataType>> {
+        let val: String = val.to_string();
+        let types: Vec<DataType> = val
+            .split(',')
+            .map(|e: &str| {
+                DataType::from_string(e.to_owned(), Util::identify_type(&e.to_owned())).unwrap()
+            })
+            .collect();
+
+        let imbalance = types.iter().find(|e| match e {
+            DataType::Symbol(ref val) => false,
+            _ => true,
+        });
+        if imbalance.is_none() {
+            Some(types)
+        } else {
+            Logger::error(
+                format!("channel values not correctly in: {:?}", imbalance.unwrap()).as_str(),
+            );
+            None
+        }
+    }
+
+    pub fn get_expressions<T: ToString>(val: T) -> Vec<BinaryExpr> {
+        let val: String = val.to_string();
+        val.split(',')
+            .map(|e| {
+                Rule::split_expression(e).expect(format!("Expression error in: {:?}", e).as_str())
+            })
+            .collect()
+    }
+
+    pub fn get_statements<T: ToString>(val: T) -> Vec<DataVar> {
+        let val: String = val.to_string();
+        val.split(',').map(Rule::split_statement).collect()
+    }
 }
+
+trait Parser {
+    fn from_unary_func_expr<T: Into<String>>(line: T) {
+        let a = Rule::split_on_raw_group("onupdate(my_channel)(a >= 2)(a : int);onupdate");
+        Rule::get_argument_groups(a).iter().enumerate().filter_map();
+    }
+}
+
+struct ParserDefault;
+impl Parser for ParserDefault {}
 
 mod test {
-    use crate::text_processing::parser::states::Parser;
+    use crate::text_processing::parser::states::{Parser, ParserDefault};
 
     #[test]
-    fn test_from_unary_func_expr() {
-        Parser::from_unary_func_expr(
-            "onCreate('my_node','you node','you_node')( id : int ,a : int = 2323,b : text = '2323' );"
-                .to_string(),
-        );
-    }
-    #[test]
-    fn test_from_unary_func_expr_2() {
-        Parser::from_unary_func_expr(
-            "onUpdate('my_node','you_node','you_node')(a >= 2, b == 2)( id : int ,a : int = 2323,b : text = '2323' );"
-                .to_string(),
-        );
+    fn test_from_unary_func_expr() -> Result<(), ()> {
+        let a = ParserDefault::from_unary_func_expr("onUpdate(my_channel)(a<=2)(b : real = 44.0)");
+        Ok(())
     }
 }
